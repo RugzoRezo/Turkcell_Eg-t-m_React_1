cee32c5c20664c6f13e157ddae07f0e10bf68980
Hakan ÖZER <hakanozer02@gmail.com>
2022-09-13 14:10:34 +0300

    Update Login.tsx

--- twodays/src/Login.tsx
+++ twodays/src/Login.tsx
@@ -1,9 +1,23 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
 
+    const navigate = useNavigate()
+
+    const [remember, setRemember] = useState(false)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const userLogin = (evt:React.FormEvent) => {
+        evt.preventDefault()
+        if ( email === 'ali@mail.com' && password === '12345' ) {
+            navigate('/dashboard')
+            //window.location.href= '/dashboard'
+        }else {
+            setError('Username or password fail!')
+        }
+    }
 
   return (
     <>
@@ -11,7 +25,15 @@ function Login() {
             <div className='col-sm-4'></div>
             <div className='col-sm-4'>
                 <h2>User Login</h2>
-                <form>
+
+                { error !== '' && 
+                    <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> { error }
+                    <button onClick={() => setError('')} type="button" className="btn-close"></button>
+                    </div>
+                }
+
+                <form onSubmit={userLogin}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                     <input onChange={(evt) => setEmail(evt.target.value) }  required type="email" className="form-control" id="exampleInputEmail1" />
@@ -21,7 +43,7 @@ function Login() {
                     <input onChange={(evt) => setPassword(evt.target.value) } required type="password" className="form-control" id="exampleInputPassword1" />
                 </div>
                 <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <input onClick={() => setRemember(!remember)} type="checkbox" className="form-check-input" id="exampleCheck1" />
                     <label className="form-check-label" htmlFor="exampleCheck1">Remember</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
